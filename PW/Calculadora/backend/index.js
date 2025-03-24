c8cba8b6079ae873280fc06407eab83507ddc66e
isa <143816039+isaxsccp@users.noreply.github.com>
2025-03-24 07:51:34 -0300

    index.js

--- PW/Calculadora/backend/index.js
+++ PW/Calculadora/backend/index.js
@@ -3,6 +3,11 @@
             //o uso do POST envia a requisicao pro back
             //
 
+// esse index.js serve o nosso backend da calculadora
+//aqui tem os imports da aplicacao
+            //o uso do POST envia a requisicao pro back
+            //
+
 import express from 'express';
 import path from 'path'; 
 import { fileURLToPath } from 'url';
@@ -32,6 +37,12 @@ app.post('/calcular', (req, res) => {
         res.status(400).json({ erro: 'Erro ao calcular a expressão' });
     }
 });
+
+// servidor
+app.listen(port, () => {
+    console.log(`Servidor backend rodando na porta ${port}`);
+});
+
 
 // servidor
 app.listen(port, () => {
